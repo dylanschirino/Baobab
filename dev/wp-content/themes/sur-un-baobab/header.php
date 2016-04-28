c6e38fe373463a6b426eafd441f938216cfd7588
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-04-28 12:06:08 +0200

    Added development

---
+++ dev/wp-content/themes/sur-un-baobab/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php bloginfo('name');?></title>
+  </head>
+  <body>
+
+<h1 class="titre">
+
+  <?php bloginfo('name');?>
+</h1>
+
+<p class="slogan">
+
+    <?php bloginfo('description');?>
+</p>
---
+++ dev/wp-content/themes/sur-un-baobab/index.php
@@ -0,0 +1,58 @@
+
+<?php
+/*
+Template Name: Homepage
+*/
+
+get_header();
+?>
+<main class="ma-page">
+      <section class="ma-page__content">
+        <?php
+        if ( have_posts() ):
+          while ( have_posts() ):
+            the_post();
+            ?>
+        <h2 class="ma-page__title"><?php the_title();?></h2>
+        <div class="ma-page__text">
+          <?php the_content();?>
+        </div>
+        <aside class="about">
+          <h3 class="about__title"><?php the_field('about_title');?> </h3>
+          <div class="about__content">
+            <?php the_field('about_content');?>
+          </div>
+        </aside>
+        <section class="last-article">
+          <h3 class="last-article__title">Dernier article</h3>
+<?php endwhile;
+endif;?>
+          <div class="last-article__container">
+            <?php
+            $args = array( 'posts_per_page' => 2 );
+            $myposts = get_posts( $args );
+            foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+        <article class="article">
+          <h4 class="article__title">
+            <?php the_title();?>
+          </h4>
+          <p class="article__date">
+            Publié le <?php the_date('l j F');?></p>
+            <p class="article__excerpt">
+              <?php the_custom_excerpt();?>
+            </p>
+            <p class="article__author">
+              <?php the_author(); ?>
+            </p>
+            <a href="<?php the_permalink();?>" class="article__link">Lire la suite</a>
+          </p>
+        </article>
+          <?php endforeach;
+          wp_reset_postdata();?>
+        </div>
+
+      </section>
+    </section>
+  </main>
+<?php
+get_footer();
